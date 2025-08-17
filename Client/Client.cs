a01669cf33d85650a331f48ce9f6280da0c6b24b
Bojana Glumac <bojanaglumac21@gmail.com>
2025-08-17 23:00:17 +0200

    UDP komunikacija izmedju klijenta i servera, klasa Client za podatke o klijentu

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +15,80 @@ namespace Client
     {
         static void Main(string[] args)
         {
+
+            #region Konekcija na server
+            Console.Title = "Client";
+
+            Console.WriteLine("Instant-messaging application! WELCOME!");
+            Socket clientSockeUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint destinationEP = new IPEndPoint(IPAddress.Loopback, 50000);
+            EndPoint sourceEP = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] recievedBuffer = new byte[65000];
+            byte[] sendBuffer;
+            bool logged = false;
+            List<string> channels = new List<string>();
+
+            try
+            {
+                while (!logged)
+                {
+                    //Unos podataka klijenta
+                    Console.Write("Enter your username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Enter your password: ");
+                    string password = Console.ReadLine();
+                    
+                    string clientData = username + '|' + password;
+                    sendBuffer = Encoding.UTF8.GetBytes(clientData);
+
+                    try
+                    {
+                        //slanje serveru login podatke
+                        clientSockeUDP.SendTo(sendBuffer, destinationEP);
+
+                        int numBytes = clientSockeUDP.ReceiveFrom(recievedBuffer, ref sourceEP);
+                        string serverMessage = Encoding.UTF8.GetString(recievedBuffer, 0, numBytes);
+
+                        if (serverMessage.Contains("Try again"))
+                        {
+                            Console.WriteLine($"Error: {serverMessage}\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine(serverMessage);
+                            logged = true;
+                        }
+
+                        Console.WriteLine("Choose a chat channel you would like to use:");
+                        numBytes = clientSockeUDP.ReceiveFrom(recievedBuffer, ref sourceEP);
+                        serverMessage = Encoding.UTF8.GetString(recievedBuffer, 0, numBytes);
+
+                        List<string> chatChannels = serverMessage.Split(',').ToList();
+                        foreach(var ch in chatChannels)
+                        {
+                            Console.WriteLine($"- {ch}");
+                        }
+
+                        Console.Write("Channel name: ");
+                        string chosenChannel = Console.ReadLine();
+
+                        sendBuffer = Encoding.UTF8.GetBytes(chosenChannel);
+                        clientSockeUDP.SendTo(sendBuffer, destinationEP);
+
+                    }
+                    catch(SocketException ex)
+                    {
+                        Console.WriteLine($"An error occurred while trying to send/recieve message\nError: {ex.Message}");
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("An error occcurred: " + ex.Message);
+            }
+
+            #endregion
+
         }
     }
 }
--- Instant-messaging_application/Server.cs
+++ Instant-messaging_application/Server.cs
@@ -1,15 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using ClientData;
 
 namespace Instant_messaging_application
 {
     internal class Server
     {
+
+        public static List<Client> clients = new List<Client>();
+        public static List<string> chatChannels = new List<string>()
+        {
+            "General",
+            "Gaming",
+            "ChillZone",
+            "Music"
+        };
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Instant-messaging application server!");
+
+            #region Logovanje klijenta na server
+
+            Socket serverSocketUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50000);
+            serverSocketUDP.Bind(serverEP);
+            EndPoint sourceEP = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine($"Server initialized and waiting on: {serverEP}");
+
+            byte[] recievedBuffer = new byte[65000];
+            byte[] sendMessage;
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        int numBytes = serverSocketUDP.ReceiveFrom(recievedBuffer, ref sourceEP);
+                        string message = Encoding.UTF8.GetString(recievedBuffer, 0, numBytes);
+
+                        //format prijave korisnika je "username|password"
+                        string[] partsOfLogin = message.Split('|');
+
+                        if(partsOfLogin.Length != 2)
+                        {
+                            Console.WriteLine($"Fomrat error on {sourceEP}");
+                            sendMessage = Encoding.UTF8.GetBytes("The login format does not match \"username|password\" format. Try again.");
+
+                            serverSocketUDP.SendTo(sendMessage, sourceEP);
+                        }
+                        
+                        string username = partsOfLogin[0];
+                        string password = partsOfLogin[1];
+
+
+                        //work in progress
+                        //Client existingClient = clients.FirstOrDefault(c => c.Username == username && c.Password == password);
+
+                        //if(existingClient == null)
+                        //{
+                        //    Client newClient = new Client(username, password, true);
+                        //    clients.Add(newClient);
+                        //    Console.WriteLine($"Added new client: {username}:{password} at {sourceEP}");
+                        //}
+                        //else
+                        //{
+                        //    Console.WriteLine("The client is already logged in!")
+                        //}
+
+                        Console.WriteLine($"Successful login from: {sourceEP} user. ====> Username: {username}; Password: {password}");
+                        sendMessage = Encoding.UTF8.GetBytes($"Successful login! Welcome {username}!");
+                        serverSocketUDP.SendTo(sendMessage, sourceEP);
+
+                        Console.WriteLine("Sending available channels to choose from...");
+
+                        //slanje kanala koji su opcija za biranje
+                        string channelsStr = string.Join(",", chatChannels);
+                        sendMessage = Encoding.UTF8.GetBytes(channelsStr);
+                        serverSocketUDP.SendTo(sendMessage, sourceEP);
+
+
+                        numBytes = serverSocketUDP.ReceiveFrom(recievedBuffer, ref sourceEP);
+                        message = Encoding.UTF8.GetString(recievedBuffer, 0, numBytes);
+                        Console.WriteLine($"User {username}:{password} has chosen the {message} channel.");
+
+                        
+
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine($"An error occured while trying to log in to server!\nError message: {ex.Message}");
+                    }
+                }
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine($"An error occurred:\n{ex.Message}");
+            }
+
+            #endregion
         }
     }
 }
